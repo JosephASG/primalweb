22eef37dc3b48866e223641998d331107c302680
Primal <alexis200123@outlook.es>
2022-04-09 04:52:25 -0500

    a

--- primalweb/settings.py
+++ primalweb/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--v%jx0$g*abggjne7v#qjz!$trpdm1r+3f52bd_m)xom+)5g+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
