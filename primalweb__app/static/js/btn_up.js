545d59e60d66730d20b922f40525d2d9e4a1438e
Primal <alexis200123@outlook.es>
2022-04-11 16:16:25 -0500

    si

--- primalweb__app/static/js/btn_up.js
+++ primalweb__app/static/js/btn_up.js
@@ -6,6 +6,17 @@ window.onscroll = function(){
         document.querySelector(".go__top__container")
         .classList.remove("show");
     }
+
+    var position = window.pageYOffset || document.documentElement.scrollTop;
+
+    var elemento_1 = document.getElementById("element_uno");
+    var elemento_2 = document.getElementById("element_dos");
+    var elemento_3 = document.getElementById("element_tres");
+    
+    elemento_1.style.bottom = position * 0.1+ "px";
+    elemento_2.style.top = position * 0.2 + "px";
+    elemento_3.style.bottom = position * 0.1+ "px";
+
 }
 
 document.querySelector(".go__top__container").addEventListener("click", () =>{
