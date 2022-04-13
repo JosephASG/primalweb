bcc1416efe046debedfdc4b5922fc334bb86a201
Primal <alexis200123@outlook.es>
2022-04-13 15:19:16 -0500

    mouse scroll animation added and athor changes

--- primalweb__app/static/js/btn_up.js
+++ primalweb__app/static/js/btn_up.js
@@ -1,4 +1,4 @@
-window.onscroll = function(){
+/*window.onscroll = function(){
     if(document.documentElement.scrollTop > 110){
         document.querySelector(".go__top__container")
         .classList.add("show");
@@ -6,7 +6,7 @@ window.onscroll = function(){
         document.querySelector(".go__top__container")
         .classList.remove("show");
     }
-    /*var position = window.pageYOffset || document.documentElement.scrollTop;
+    var position = window.pageYOffset || document.documentElement.scrollTop;
 
     var elemento_1 = document.getElementById("element_one");
     var elemento_2 = document.getElementById("element_dos");
@@ -16,13 +16,13 @@ window.onscroll = function(){
     
     elemento_2.style.top = position * 0.1 + "px";
     elemento_4.style.top = position * 0.2 + "px";
-    elemento_5.style.top = position * 0.2 + "px";*/
+    elemento_5.style.top = position * 0.2 + "px";
 }
 
-document.querySelector(".go__top__container").addEventListener("click", () =>{
+/*document.querySelector(".go__top__container").addEventListener("click", () =>{
     window.scroll({
         top: 0,
         behavior: "smooth"
     });
-});
+});*/
 
